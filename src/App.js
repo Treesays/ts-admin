da7f2df3a4c61a20518e1c126239fb37794ff620
n8659150 <jackli900421@hotmail.com>
2020-01-04 23:36:11 +0800

    navbar and footer draft

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { FrontendAuth }  from './components/RouterComponents';
-import {
-    HashRouter,
-    Switch,
-} from "react-router-dom";
+import { FrontendAuth } from "./components/RouterComponents";
+import Navbar from "./components/Navbar";
+import { HashRouter, Switch } from "react-router-dom";
 import { routerConfig } from "./routers";
-export class App extends React.Component{
-  render(){
-      return(
-          <HashRouter>
-              <Switch>
-                  <FrontendAuth config={routerConfig} />
-              </Switch>
-          </HashRouter>
-      );
-  }
+export class App extends React.Component {
+    render() {
+        const currentUserName = AV.User.current()
+            ? AV.User.current()["attributes"]["username"]
+            : null;
+        return (
+            <HashRouter>
+                {currentUserName && <Navbar username={currentUserName} />}
+                <Switch>
+                    <FrontendAuth
+                        config={routerConfig}
+                        isAuthed={currentUserName}
+                    />
+                </Switch>
+                {currentUserName && <footer>2019 Powered by Jack Li</footer>}
+            </HashRouter>
+        );
+    }
 }
